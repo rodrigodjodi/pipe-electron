4b963709f43982396b34ce72d922d9afc97018b4
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-10-26 18:47:34 -0300

    Initial commit.

---
+++ src/pages/ItemView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{itensProjetoCorrente[$route.params.item].nome}}</h1>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+  name: "ItemView",
+  computed: {
+    ...mapState(["itensProjetoCorrente"])
+  },
+  created() {}
+};
+</script>
+
+<style>
+</style>
