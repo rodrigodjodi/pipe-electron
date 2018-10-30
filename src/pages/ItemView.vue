91f6ba46d71a786dcf3268fc2dde1291e13d7720
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-10-30 18:35:25 -0300

    Formatação da tabela de tarefas.

--- src/pages/ItemView.vue
+++ src/pages/ItemView.vue
@@ -4,11 +4,49 @@
     <v-data-table
       :items="tarefasProjetoCorrente.filter(tarefa => tarefa.item === idItem)"
       class="elevation-1"
+      hide-actions
+      hide-headers
     >
       <template slot="items" slot-scope="props">
-        <td>{{ props.item.text }}</td>
-        <td class="text-xs-right">{{ props.item.item }}</td>
+        <td class="px-3">
+          <v-checkbox
+            :input-value="props.item.done"
+            hide-details
+             
+          ></v-checkbox>
+        </td>
+        <td class="px-2" @click="editTarefa">{{ props.item.text }}</td>
+        <td class=" px-0">
+          <v-icon
+            small
+            class="mr-2"
+            
+          >
+            access_time
+          </v-icon>
+          <v-icon
+            small
+            class="mr-2"
+          >
+            delete
+          </v-icon>
+        </td>
         
+      </template>
+      <template slot="footer">
+        <td colspan="100%">
+          <v-text-field
+          solo
+          clearable
+          flat
+          placeholder="Adicionar tarefa..."
+          v-model="tarefa.text"
+          hide-details
+          @keyup.enter="novaTarefa"
+          >
+
+          </v-text-field>
+        </td>
       </template>
     </v-data-table>
   </v-layout>
@@ -22,7 +60,12 @@ export default {
   props: ["idItem", "idProjeto"],
   data() {
     return {
-      item: {}
+      item: {},
+      tarefa: {
+        text: "",
+        item: this.idItem,
+        done: false
+      }
     };
   },
   computed: {
@@ -34,6 +77,26 @@ export default {
       this.item = doc;
       this.$store.commit("SET_TITLE", doc.nome);
     });
+  },
+  methods: {
+    novaTarefa() {
+      let payload = {
+        path: `projetos/${this.idProjeto}/tarefas`,
+        tarefa: this.tarefa
+      };
+      this.$store
+        .dispatch("novaTarefa", payload)
+        .then(docRef => {
+          this.tarefa.text = "";
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(error => {
+          console.error("Error adding document: ", error);
+        });
+    },
+    editTarefa() {
+      console.log("editing");
+    }
   }
 };
 </script>
